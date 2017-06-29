c1530373762f410ad376d932e276d24a9b219179
Joshua Tompkins <jtompkins@newrelic.com>
2017-06-28 19:33:57 -0700

    Add tests for SetRepository

--- src/main/kotlin/io/sylvanlibrary/api/models/Set.kt
+++ src/main/kotlin/io/sylvanlibrary/api/models/Set.kt
@@ -3,13 +3,15 @@ package io.sylvanlibrary.api.models
 import org.skife.jdbi.v2.StatementContext
 import org.skife.jdbi.v2.tweak.ResultSetMapper
 import java.sql.ResultSet
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 data class Set(
   val Id: Int,
   val Name: String,
   val Abbr: String,
-  val ReleaseDate: Date,
+  val ReleaseDate: LocalDate,
   val IconUrl: String?,
   val PromoArtUrl: String?
 )
@@ -20,7 +22,7 @@ class SetResultSetMapper : ResultSetMapper<Set> {
       r!!.getInt("Id"),
       r.getString("Name"),
       r.getString("Abbr"),
-      r.getDate("ReleaseDate"),
+      LocalDate.parse(r.getString("ReleaseDate"), DateTimeFormatter.ISO_DATE),
       r.getString("IconUrl"),
       r.getString("PromoArtUrl"))
 
